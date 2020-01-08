e566f397bce2ca137a77fc928326cea03d55fe87
Automate 6500 <automate6500@gmail.com>
2020-01-08 10:49:12 -0800

    Yep

--- .github/action-check/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -xe
-# Requirements (Stage 1)
-pip install --quiet --upgrade pip
-pip install --quiet --requirement requirements.txt
-
-# Checks (Stage 2)
-flake8 --ignore=E501,E231 *.py
-pylint --disable=C0301 --disable=C0326 *.py
-python -m unittest --verbose --failfast
